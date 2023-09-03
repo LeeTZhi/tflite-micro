10bc7b38ecd93e290809655b50f3c0d1924e287f
ltz <azhi207@gmail.com>
2023-09-03 16:15:50 +0800

    MOD: build scritps

--- build_arm_m33.sh
+++ build_arm_m33.sh
@@ -1,9 +1,9 @@
 #!/bin/bash
-BUILD_TYPE := release
-make -f tensorflow/lite/micro/tools/make/Makefile TARGET=cortex_m_generic TARGET_ARCH=cortex-m33 \
+BUILD_TYPE=release
+gmake -f tensorflow/lite/micro/tools/make/Makefile TARGET=cortex_m_generic TARGET_ARCH=cortex-m33 \
     OPTIMIZED_KERNEL_DIR=cmsis_nn BUILD_TYPE=$BUILD_TYPE microlite  -j4
-make -f tensorflow/lite/micro/tools/make/Makefile TARGET=cortex_m_generic TARGET_ARCH=cortex-m33 \
+gmake -f tensorflow/lite/micro/tools/make/Makefile TARGET=cortex_m_generic TARGET_ARCH=cortex-m33 \
     OPTIMIZED_KERNEL_DIR=cmsis_nn BUILD_TYPE=$BUILD_TYPE lib_mfcc  -j4
-make -f tensorflow/lite/micro/tools/make/Makefile TARGET=cortex_m_generic TARGET_ARCH=cortex-m33 \
+gmake -f tensorflow/lite/micro/tools/make/Makefile TARGET=cortex_m_generic TARGET_ARCH=cortex-m33 \
     OPTIMIZED_KERNEL_DIR=cmsis_nn BUILD_TYPE=$BUILD_TYPE lib_snore_detection -j4
 
